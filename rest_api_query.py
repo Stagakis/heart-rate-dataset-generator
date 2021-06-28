8a674132477c9222a2cb86946e461f0764efa5c2
Stagakis <nick-stag@hotmail.com>
2021-06-28 21:43:31 +0300

    create base script for querying the database

---
+++ rest_api_query.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+login = json.load(open("login_info.json"))          # These 2 json files are not included 
+database = json.load(open("heart_rate_info.json"))  # in the repo for privacy reasons
+
+login_url = database['login']
+heartrate_data_url = database['heart_rate_all']
+
+user = login['email'].split('@')[0] #Get the user for folder creation
+response = requests.post(login_url, data=login)
+access_token = response.json()['data']['access_token']
+
+headers = {"Authorization": "Bearer " + access_token}
+response = requests.get(heartrate_data_url, headers=headers)
+
+print(response.json())
