7ea3b5d51680336c4c7683d94f6bbd630eac170a
Stagakis <nick-stag@hotmail.com>
2021-10-05 15:23:34 +0300

    Add functionality of getting activity and questionnaires

--- data_getters.py
+++ data_getters.py
@@ -1,8 +1,10 @@
 import requests
 import json
 import os
+import time
 
 login_info = json.load(open("login_info.json"))
+api_info = json.load(open("api_urls.json"))
 
 
 def user_login(email, password):
@@ -11,20 +13,35 @@ def user_login(email, password):
     access_token = response.json()['data']['access_token']
     return access_token
 
+def get_activity(token, activity_type, from_to_tuple=None):
+    if from_to_tuple is None:  # Get all activity data
+        activity_data_url = api_info["heart_rate_all"].replace("{api}", login_info["api"])
+        response = requests.get(activity_data_url, headers={"Authorization": "Bearer " + token})
+        return response
+    else:
+        pass
 
 def get_heartrate(token, from_to_tuple=None):
     if from_to_tuple is None:  # Get all heartrate data
-        headers = {"Authorization": "Bearer " + token}
-        heartrate_data_url = login_info["api"] + "/login"
-        response = requests.get(heartrate_data_url, headers=headers)
-        pass
+        heartrate_data_url = api_info["heart_rate_all"].replace("{api}", login_info["api"])
+        response = requests.get(heartrate_data_url, headers={"Authorization": "Bearer " + token})
+
     else:
         start, end = from_to_tuple
-
         pass
 
-
+def get_questionaire(token, from_to_tuple=None):
+    if from_to_tuple is None: # Get latest
+        questionaire_data_url = api_info["questionanaires"].replace("{api}", login_info["api"]).replace("{questionnaire}", "big_five")
+        response = requests.get(questionaire_data_url, headers={"Authorization": "Bearer " + token})
+        return response
+    else:
+        start, end = from_to_tuple
+        pass
 if __name__ == "__main__":
     token = user_login(login_info["email"], login_info["password"])
 
-    get_heartrate(token, (0, 1000))
+    get_heartrate(token)
+    activity = get_activity(token, "walking", (0, 1623929554791))
+    quest = get_questionaire(token)
+    pass
